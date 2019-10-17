fbd3812b52bc5522774198820d96c75a0d116a78
Jair Avilés <hi@jairaviles.mx>
2019-10-16 22:07:17 -0700

    init commit

---
+++ src/test/java/com/oreilly/reactiveofficers/ReactiveOfficersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oreilly.reactiveofficers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveOfficersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
